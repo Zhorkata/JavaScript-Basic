14d7df64f36d7af16b36fba5401e98cb0d3d5117
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-19 10:48:59 +0300

    Update 05. Number 100...200.js

--- 02. Conditional Statements/05. Number 100...200.js
+++ 02. Conditional Statements/05. Number 100...200.js
@@ -2,9 +2,9 @@ function number(input) {
     let num = Number(input[0]);
 
     if (num < 100) {
-        console.log("Less than 100");
+        console.log("Less than 100");//100, 99, 98...
     }
-    else if (num >= 100, num <= 200) {
+    else if (num >= 100, num <= 200) { //101,..., 199
         console.log("Between 100 and 200")
     } else {
         console.log("Greater than 200")
