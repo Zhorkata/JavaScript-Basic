b781e2c74a85ea59cb0ce00d58789732592cdf65
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-19 10:48:06 +0300

    Update 03. Even or Odd.js

--- 02. Conditional Statements/03. Even or Odd.js
+++ 02. Conditional Statements/03. Even or Odd.js
@@ -1,7 +1,7 @@
 function evenrOrOdd(input) {
     let num = Number(input[0]);
 
-    if (num % 2 === 0) {
+    if (num % 2 === 0) { 
         console.log("even");
     } else {
         console.log("odd")
